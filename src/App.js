14b757a1f1fd52d51c7411dfbb2e8dc2cf6eec1c
Dnyaneshwar Taur <dnyand33p@gmail.com>
2022-08-14 00:30:39 +0530

    changing letters on viewport

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { render } from "@testing-library/react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentLetter: 0,
+      letters: ["र", "ज", "त", "ब"],
+    };
+  }
+  drawLetter(direction) {
+    let letters = this.state.letters.slice();
+    console.log(direction);
+    let currentLetter = this.state.currentLetter;
+    let nextLetter;
+    if (direction === "next") {
+      nextLetter = currentLetter + 1;
+    } else {
+      if (currentLetter === 0) {
+        nextLetter = letters.length - 1;
+      } else {
+        nextLetter = currentLetter - 1;
+      }
+    }
+    console.log(nextLetter);
+    if (nextLetter >= letters.length) {
+      nextLetter = 0;
+    }
+    console.log(nextLetter);
+    this.setState({ currentLetter: nextLetter, letters: letters });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="page">
+          <h1>Learn to Read Marathi</h1>
+          <div className="letterBoard">
+            <div className="letter">
+              {this.state.letters[this.state.currentLetter]}
+            </div>
+          </div>
+          <div class="controller">
+            <button onClick={() => this.drawLetter("pervious")}>
+              Previous
+            </button>
+            <button onClick={() => this.drawLetter("next")}>Next</button>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/letterDisplay.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+class Letter extends React.Component {
+  render() {
+    return <div className="">{this.props.currentLetter}</div>;
+  }
+}
